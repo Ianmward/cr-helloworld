f37e195b38c21cade89399becfef2cbc02f43c5c
Ian Ward <Ianmward@gmail.com>
2022-05-16 13:12:25 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
+  const name = process.env.NAME || 'Everyone';
   res.send(`Hello ${name}!`);
 });
 app.listen(port, () => {
